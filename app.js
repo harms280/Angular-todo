ca9df4f940f7beef7db4dac19a4bcc1314745e85
aaron <harms280@gmail.com>
2015-11-13 11:24:41 -0800

    finish assessment

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-app = angular.module('todoApp'); // What's missing? Hint, it's more than one thing.
+var app = angular.module('todoApp', []);
 
-app.controller("TodosController", function() { // What's missing?
-	$scope.todos = ; // Store todo items here. What is the data type?
-	function addTodo() { // Rewrite to make this function abailable in your view
+app.controller("TodosController", function($scope) { 
+	$scope.todos = []; // Store todo items here. What is the data type?
+  $scope.newTodo = {};
+  $scope.visible = false;
+
+	$scope.addTodo = function (newTodo) { // Rewrite to make this function abailable in your view
 		// Add code here
+    $scope.todos.push(newTodo);
+    $scope.newTodo = {};
+    $scope.todoForm.$setPristine();
+    $scope.visible = false;
+
 	};
-	function deleteAll() { // Rewrite to make this function abailable in your view
+	$scope.deleteAll = function () { // Rewrite to make this function abailable in your view
 		// Add code here
+    $scope.todos = [];
 	};
+  $scope.showForm = function() {
+    $scope.visible = true;
+  };
 });
